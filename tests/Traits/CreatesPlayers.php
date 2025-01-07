2ea80d77af45ea868178c0505cfe9e0bfae953bd
aghorianducalis <aghorianducalis@gmail.com>
2025-01-07 09:50:39 +0200

    Add PlayerBrawlerDTO

--- tests/Traits/CreatesPlayers.php
+++ tests/Traits/CreatesPlayers.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Tests\Traits;
 
-use App\API\DTO\Response\PlayerDTO;
+use App\API\DTO\Response\ClubPlayerDTO;
 use App\Models\Player;
 
 trait CreatesPlayers
@@ -14,7 +14,6 @@ trait CreatesPlayers
         $this->assertNotNull($playerActual);
         $this->assertInstanceOf(Player::class, $playerActual);
         $this->assertSame($playerExpected->id, $playerActual->id);
-        $this->assertSame($playerExpected->ext_id, $playerActual->ext_id);
         $this->assertSame($playerExpected->tag, $playerActual->tag);
         $this->assertSame($playerExpected->name, $playerActual->name);
         $this->assertSame($playerExpected->club_id, $playerActual->club_id);
@@ -34,12 +33,12 @@ trait CreatesPlayers
         );
     }
 
-    private function assertPlayerModelMatchesDTO(Player $player, PlayerDTO $playerDTO): void
+    private function assertPlayerModelMatchesDTO(Player $player, ClubPlayerDTO $playerDTO): void
     {
         $this->assertSame($player->tag, $playerDTO->tag);
         $this->assertSame($player->name, $playerDTO->name);
         $this->assertSame($player->name_color, $playerDTO->nameColor);
-//        $this->assertSame($player->role, $playerDTO->role);//todo
+        $this->assertSame($player->role ?? '', $playerDTO->role);//todo
         $this->assertSame($player->trophies, $playerDTO->trophies);
         $this->assertSame($player->icon_id, $playerDTO->icon['id']);//todo
     }
